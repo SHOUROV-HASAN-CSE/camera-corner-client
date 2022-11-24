b6ca36ca46a0954dc4ec002b1c17f578075c8ce8
Shourov Hasan <shourovhasan172@gmail.com>
2022-11-24 14:03:15 +0600

    login page created

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
 
 
 
@@ -13,7 +14,11 @@ const router = createBrowserRouter([
       {
         path:'/',
         element:<Home/>
-      }
+      },
+      {
+        path:'/login',
+        element:<Login/>
+      },
     ]
   }
 ])
