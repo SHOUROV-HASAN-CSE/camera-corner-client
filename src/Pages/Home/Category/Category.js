e189805847558342a4fe65227812e1faa19b3fb6
Shourov Hasan <shourovhasan172@gmail.com>
2022-11-26 02:14:01 +0600

    used camera category replace

--- src/Pages/Home/Category/Category.js
+++ src/Pages/Home/Category/Category.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import canon from '../../../assets/images/category/canon.png';
+import nikon from '../../../assets/images/category/nikon.jpg';
+import sony from '../../../assets/images/category/sony.png';
 
 const Category = () => {
   return (
     <div className=' text-5xl text-center my-8'>
       <h1>Used Camera Category</h1>
-      <div className='grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-center mx-auto w-1/4'>
-        
-      <Link to='/categories'><label className="btn">Sony</label></Link>
-      <Link to='/categories'><label className="btn">Canon</label></Link>
-      <Link to='/categories'><label className="btn">Nikon</label></Link>
-      
-      
+    
+      <div className='grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-center mx-auto w-2/4'>
+     
+      <Link to='/categories'><img src={canon} alt="" /></Link>
+      <Link to='/categories'><img src={nikon} alt="" /></Link>
+      <Link to='/categories'><img className=' mt-12' src={sony} alt="" /></Link>
+
       </div>
       
     </div>
