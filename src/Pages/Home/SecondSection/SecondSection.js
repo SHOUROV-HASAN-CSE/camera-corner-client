09d2c14e80b42ebafbb211a2bca5fa003caf8ec0
Shourov Hasan <shourovhasancse@gmail.com>
2023-05-09 03:19:07 -0700

    image optimize

--- src/Pages/Home/SecondSection/SecondSection.js
+++ src/Pages/Home/SecondSection/SecondSection.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import img1 from '../../../assets/images/image/image-3.PNG';
-import img2 from '../../../assets/images/image/image-4.PNG';
-import img3 from '../../../assets/images/image/image-5.PNG';
+import img1 from '../../../assets/images/image/image-3.png';
+import img2 from '../../../assets/images/image/image-4.png';
+import img3 from '../../../assets/images/image/image-5.png';
 
 const SecondSection = () => {
   return (
@@ -14,3 +14,5 @@ const SecondSection = () => {
 };
 
 export default SecondSection;
+
+
